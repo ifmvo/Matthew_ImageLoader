287477839d9fbe40e1779e73cfcb6584f0192550
Matthew_Chen <Matthew_Chen_1994@163.com>
2018-10-15 09:37:33 +0800

    添加readme

--- app/src/main/java/com/ifmvo/testLoader/MainActivity.kt
+++ app/src/main/java/com/ifmvo/testLoader/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.ifmvo.testLoader
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.ifmvo.imageloader.ILFactory
+import com.ifmvo.imageloader.LoadListener
 import com.ifmvo.imageloader.progress.LoaderOptions
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,7 +15,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ILFactory.getLoader().load(this, image, "https://github.com/ifmvo/SomeImage/blob/master/banner3.png?raw=true", LoaderOptions().defaultOptions())
+
+        val url = "https://github.com/ifmvo/SomeImage/blob/master/banner3.png?raw=true"
+
+        ILFactory.getLoader().load(this, imageview, url, LoaderOptions().defaultOptions())
+
+        ILFactory.getLoader().load(this, imageview, url, LoaderOptions().placeHolder(R.mipmap.ic_launcher).error(R.mipmap.ic_launcher_round))
+
+        ILFactory.getLoader().load(this, imageview, url, LoaderOptions().placeHolder(R.mipmap.ic_launcher).error(R.mipmap.ic_launcher_round).skipCache())
+
+        ILFactory.getLoader().load(this, imageview, url, LoaderOptions().placeHolder(R.mipmap.ic_launcher).error(R.mipmap.ic_launcher_round).skipCache().circle())
+
+        ILFactory.getLoader().load(this, imageview, url, LoaderOptions().placeHolder(R.mipmap.ic_launcher).error(R.mipmap.ic_launcher_round).skipCache().circle(), object : LoadListener(){
+            override fun onLoadCompleted(drawable: Drawable?): Boolean {
+                return false
+            }
+
+            override fun onLoadFailed(e: Throwable?): Boolean {
+                return super.onLoadFailed(e)
+            }
+
+            override fun onLoadProgress(progress: Int) {
+                super.onLoadProgress(progress)
+            }
+        })
 
     }
 }
